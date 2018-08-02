447595d9252e843bb3d752df6418b4b96d3aadc1
daqingzhang <daqingzhang@bestechnic.com>
2018-08-02 14:27:05 +0800

    target/robot-a ok

---
+++ target/robot-a/source/app_main.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <common.h>
+
+int app_main(void)
+{
+	printf("hello world\n");
+
+	for(;;) {
+		//TODO: do sth
+	}
+
+	return 0;
+}
---
+++ target/robot-a/source/config.mk
@@ -0,0 +1,16 @@
+###########################################################
+# TOPDIR:	top directory of project
+# INC:		directory of header file
+# LIB:		library name will be generated
+# AOBJS:	object file name by assembler source file
+# COBJS:	object file name by c source file
+
+###########################################################
+INC		:=-I$(TOPDIR)/common/inc
+INC		+=-I$(TOPDIR)/arch/$(ARCH)/$(CPU)/$(VENDOR)/$(CHIP)/lib/inc
+INC		+=-I$(TOPDIR)/$(TARGET)/source
+
+LIB		:=libtgt.a
+
+AOBJS	:=$(patsubst %.S,%.o,$(wildcard *.S))
+COBJS	:=$(patsubst %.c,%.o,$(wildcard *.c))
